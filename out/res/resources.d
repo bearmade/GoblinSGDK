1159d1b84bcf71bfb32165136dd711a8426db620
Hunter Singleton <bear.made.games@gmail.com>
2025-01-23 10:41:19 -0600

    added out folder(removed from .gitignore)

---
+++ out/res/resources.d
@@ -0,0 +1,10 @@
+out/res/resources.o: res/resources.res \
+/home/bear/GoblinSGDK/res/tiles2.png \
+/home/bear/GoblinSGDK/res/tilesSEGA2.png \
+/home/bear/GoblinSGDK/res/tileset_1.png \
+/home/bear/GoblinSGDK/res/swoosh.wav \
+/home/bear/GoblinSGDK/res/croakusTitleSEGA.png \
+/home/bear/GoblinSGDK/res/sg_leggup_walk2.png \
+/home/bear/GoblinSGDK/res/ikag.vgm \
+/home/bear/GoblinSGDK/res/Font.png \
+/home/bear/GoblinSGDK/res/layer_1.png
